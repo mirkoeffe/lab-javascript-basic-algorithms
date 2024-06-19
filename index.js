3422373628d0efc1fe98a74817d7ac62c2a18513
Mirko Fede <mirko@mirkoeffe.shop>
2024-06-19 16:48:49 +0200

    Correct code and update minor things

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Iteration 1: Names and Input
 
 let hacker1 = "Martin";
-console.log(`"The dirver's name is ${hacker1}"`);
+console.log(`"The driver's name is ${hacker1}"`);
 
 let hacker2 = "Mirko";
 console.log(`"The navigator's name is ${hacker2}"`);
@@ -10,10 +10,10 @@ console.log(`"The navigator's name is ${hacker2}"`);
 
 if (hacker1.length > hacker2.length) {
     console.log(`The driver has the longest name, it has ${hacker1.length} characters.`);
-} else if (hacker1.length > hacker2.length) {
+} else if (hacker2.length > hacker1.length) {
     console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`);
 } else {
-    console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`)
+    console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`);
 }
 
 // Iteration 3: Loops
@@ -77,3 +77,18 @@ for (let i = 0; i < longText.length; i++) {
 }
 
 console.log(`In this paragraph there are: ${count} words`);
+
+//-----------
+
+
+const stringToCheck = "step on no pets";
+let reversedString = "";
+for (let i = stringToCheck.length - 1; i >= 0; i--) {
+    reversedString += stringToCheck[i].toLowerCase();
+}
+console.log(reversedString);
+if (stringToCheck === reversedString) {
+    console.log("String is a palindrome");
+} else {
+    console.log("Not a palindrome");
+}
